1cef6c7b65ff75f42ff0b5d00b6318126a3f08af
StephenLAB20 <stephenlab1@gmail.com>
2020-12-11 19:37:40 +0200

    Lesson 7 / + File uploading + access to static files(css, js...)

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -4,20 +4,27 @@ import com.example.sweater.domain.Message;
 import com.example.sweater.domain.User;
 import com.example.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
 
 @Controller
 public class MainController {
 
     @Autowired
     private MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting() {
@@ -40,28 +47,34 @@ public class MainController {
 
     @PostMapping("/main")
     public String sendMessage(
+            @RequestParam("file") MultipartFile file,
             @AuthenticationPrincipal User user,
-            @RequestParam String text,
-            @RequestParam String tag, Model model) {
+            @RequestParam(required = true) String text,
+            @RequestParam String tag, Model model) throws IOException {
         Message message = new Message(text, tag, user);
-        messageRepo.save(message);
-        Iterable<Message> messages = messageRepo.findAll();
-        model.addAttribute("message", messages);
-        return "main";
+        System.out.println("message " + message.getText());
+
+        if (message.getText() != null && !message.getText().isEmpty()) {
+            if (file != null && !file.getOriginalFilename().isEmpty()) {
+                File uploadDir = new File(uploadPath);
+                if (!uploadDir.exists()) {
+                    uploadDir.mkdir();
+                }
+
+                String uuidFile = UUID.randomUUID().toString();
+                String resultFileName = uuidFile + "." + file.getOriginalFilename();
+                file.transferTo(new File(uploadPath + "/" + resultFileName));
+
+                message.setFilename(resultFileName);
+            }
+
+            messageRepo.save(message);
+            Iterable<Message> messages = messageRepo.findAll();
+            model.addAttribute("message", messages);
+        }
+
+        return "redirect:/main";
     }
 
-    // transfered to @GetMapping("/main")
-//    @PostMapping("/filter")
-//    public String filter(@RequestParam String tag, Model model) {
-//        Iterable<Message> messages;
-//
-//        if(tag != null && !tag.isEmpty()) {
-//            messages = messageRepo.findByTag(tag);
-//        } else
-//            messages = messageRepo.findAll();
-//
-//        model.addAttribute("message", messages);
-//        return "main";
-//    }
 
 }
